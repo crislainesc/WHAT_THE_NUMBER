b6f33e2390d64f6819e00efb5e15289fce94617b
Crislaine Santos <crislaine.costa@arapiraca.ufal.br>
2022-02-14 19:31:27 -0300

    feat: add def parse_input and condig pattern matching

--- lib/guess.ex
+++ lib/guess.ex
@@ -10,6 +10,17 @@ defmodule Guess do
     IO.puts("Let's play What The Number!")
 
     IO.gets("Escolha um nivel de difuculdade (1, 2 ou 3): ")
+    |> Integer.parse()
+    |> parse_input()
     |> IO.inspect()
   end
+
+
+  def parse_input(:error) do
+    IO.puts("Nivel invalido!!")
+    run()
+  end
+
+  def parse_input({num, _}), do: num
+
 end
